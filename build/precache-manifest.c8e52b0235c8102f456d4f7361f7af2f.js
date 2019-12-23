cc82a0d4c5f3feb9004ab9ec6f46aafec37944fe
ray sin <kairisin@gmail.com>
2019-12-23 14:17:52 +0800

    updated

---
+++ build/precache-manifest.c8e52b0235c8102f456d4f7361f7af2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b710fa5e315c8743bc535058e8eaa6e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bcfdf039815f27e2f917",
+    "url": "/static/css/main.b5584fe1.chunk.css"
+  },
+  {
+    "revision": "50e22e9bbff0852d7ae8",
+    "url": "/static/js/2.283f9f8b.chunk.js"
+  },
+  {
+    "revision": "dd897ee3cfa67e5c2ebba71b5eb1cda6",
+    "url": "/static/js/2.283f9f8b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bcfdf039815f27e2f917",
+    "url": "/static/js/main.471bad16.chunk.js"
+  },
+  {
+    "revision": "838f2eed3c5ebcee5028",
+    "url": "/static/js/runtime-main.361f48e1.js"
+  }
+]);
